0a1a706d000c735524bd7074b36976467093d414
Shreyans Sharma <48876566+iamshreyans@users.noreply.github.com>
2023-04-15 21:34:59 -0400

    Map setup completed

--- src/main/java/de/dennisguse/opentracks/ViewTrackMap.java
+++ src/main/java/de/dennisguse/opentracks/ViewTrackMap.java
@@ -1,14 +1,63 @@
 package de.dennisguse.opentracks;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import de.dennisguse.opentracks.databinding.StatisticsRecordedEnhancementBinding;
 
 import android.os.Bundle;
 
-public class ViewTrackMap extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+public class ViewTrackMap extends AppCompatActivity implements OnMapReadyCallback {
+    private GoogleMap mMap;
+    private StatisticsRecordedEnhancementBinding binding;
+
+
+
+    // To be deleted
+    private Double originLatitude = 28.5021359;
+    // To be deleted
+    private Double originLongitude = 77.4054901;
+
+    // To be deleted
+    private Double destinationLatitude = 28.5151087;
+    private Double destinationLongitude = 77.3932163;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.statistics_recorded_enhancement);
+        binding = StatisticsRecordedEnhancementBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
+        mMap = googleMap;
+        LatLng originLocation = new LatLng(originLatitude, originLongitude);
+        mMap.clear();
+        mMap.addMarker(new MarkerOptions().position(originLocation));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(originLocation, 18F));
+
+
+    }
+
+    private String getDirectionURL( LatLng origin, LatLng dest, String secret) {
+        return "https://maps.googleapis.com/maps/api/directions/json?origin=${origin.latitude},${origin.longitude}" +
+                "&destination=${dest.latitude},${dest.longitude}" +
+                "&sensor=false" +
+                "&mode=driving" +
+                "&key=$secret";
     }
 }
