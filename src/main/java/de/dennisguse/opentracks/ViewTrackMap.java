c4b556d3dc253bf31edd686cd59128963a3ec9b9
Shreyans Sharma <48876566+iamshreyans@users.noreply.github.com>
2023-04-17 20:16:26 -0400

    Map route points integration completed.

--- src/main/java/de/dennisguse/opentracks/ViewTrackMap.java
+++ src/main/java/de/dennisguse/opentracks/ViewTrackMap.java
@@ -2,38 +2,60 @@ package de.dennisguse.opentracks;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import de.dennisguse.opentracks.data.TrackDataHub;
+import de.dennisguse.opentracks.data.models.LocationPoints;
+import de.dennisguse.opentracks.data.models.Track;
 import de.dennisguse.opentracks.databinding.StatisticsRecordedEnhancementBinding;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.util.ArrayList;
 
 
 public class ViewTrackMap extends AppCompatActivity implements OnMapReadyCallback {
     private GoogleMap mMap;
     private StatisticsRecordedEnhancementBinding binding;
-
-
-
-    // To be deleted
-    private Double originLatitude = 28.5021359;
-    // To be deleted
-    private Double originLongitude = 77.4054901;
-
-    // To be deleted
-    private Double destinationLatitude = 28.5151087;
-    private Double destinationLongitude = 77.3932163;
+    int idx = 0;
+    private Track.Id trackId;
+    private TrackDataHub trackDataHub;
+    ArrayList routeLatLong ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = StatisticsRecordedEnhancementBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        trackDataHub = new TrackDataHub(this);
+        trackDataHub.start();
+
+        Intent intent = getIntent();
+        trackId = intent.getParcelableExtra("trackId");
+
+
+
+        for (int i = 0; i < TrackRecordingActivity.locationPointsList.size(); i++) {
+
+          routeLatLong  = new ArrayList();
+            if (TrackRecordingActivity.locationPointsList.get(i).getTrackId() == trackId) {
+
+                LocationPoints point = TrackRecordingActivity.locationPointsList.get(i);
+                idx = i;
+
+                routeLatLong.add(new LatLng(point.getLatitude(), point.getLongitude()));
+                break;
+            }
+        }
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -45,19 +67,49 @@ public class ViewTrackMap extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(@NonNull GoogleMap googleMap) {
 
         mMap = googleMap;
-        LatLng originLocation = new LatLng(originLatitude, originLongitude);
         mMap.clear();
-        mMap.addMarker(new MarkerOptions().position(originLocation));
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(originLocation, 18F));
+
+        //ArrayList routeLatLong = getRouteLatLongTemp();
+
+        if (routeLatLong == null) {
+            Toast.makeText(this, "Route not found", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        LatLng positionToBeZoomedAt = (LatLng) routeLatLong.get(0);
+
+        drawRouteOnMap(getPolylineOptions(routeLatLong));
+
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(positionToBeZoomedAt, 18F));
 
 
     }
 
-    private String getDirectionURL( LatLng origin, LatLng dest, String secret) {
-        return "https://maps.googleapis.com/maps/api/directions/json?origin=${origin.latitude},${origin.longitude}" +
-                "&destination=${dest.latitude},${dest.longitude}" +
-                "&sensor=false" +
-                "&mode=driving" +
-                "&key=$secret";
+    private ArrayList getRouteLatLongTemp() {
+        ArrayList points = new ArrayList();
+        LatLng position = new LatLng(45.489763, -73.584011);
+        points.add(position);
+
+
+        position = new LatLng(45.490691, -73.582980);
+        points.add(position);
+
+        position = new LatLng(45.492120, -73.581255);
+        points.add(position);
+
+        return points;
+
+    }
+
+    private PolylineOptions getPolylineOptions(ArrayList routeLatLong) {
+        PolylineOptions lineOptions = new PolylineOptions();
+        lineOptions.addAll(routeLatLong);
+        lineOptions.width(12);
+        lineOptions.color(Color.BLUE);
+        lineOptions.geodesic(true);
+        return lineOptions;
+    }
+
+    private void drawRouteOnMap(PolylineOptions lineOptions) {
+        mMap.addPolyline(lineOptions);
     }
 }
