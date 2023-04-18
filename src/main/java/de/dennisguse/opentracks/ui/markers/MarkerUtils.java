924d4de1cdce0be41d277b7cc1231633703fb252
prince3453 <princeghoghari9@gmail.com>
2023-04-17 20:12:52 -0400

    PMD Issue : MarkerUtils.java #536

--- src/main/java/de/dennisguse/opentracks/ui/markers/MarkerUtils.java
+++ src/main/java/de/dennisguse/opentracks/ui/markers/MarkerUtils.java
@@ -22,11 +22,16 @@ import de.dennisguse.opentracks.R;
 import de.dennisguse.opentracks.data.models.Track;
 import de.dennisguse.opentracks.util.FileUtils;
 
-public class MarkerUtils {
+public final class MarkerUtils {
 
     private static final String TAG = FileUtils.class.getSimpleName();
 
+    /**
+     * Default access icon resource ID for markers.
+     * Note: Access modifier is default (no modifier specified).
+     */
     static final int ICON_ID = R.drawable.ic_marker_orange_pushpin_with_shadow;
+
     private static final String JPEG_EXTENSION = "jpeg";
 
     private MarkerUtils() {
